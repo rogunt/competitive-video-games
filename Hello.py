86cf32015d21c9f0ebaab8beb1215ab17aecea76
rogunt <132632873+rogunt@users.noreply.github.com>
2023-12-23 20:30:48 +0000

    update endpoint

--- Hello.py
+++ Hello.py
@@ -1,39 +1,28 @@
 import streamlit as st
 import requests
+import pandas as pd
 
-# Define Giant Bomb API endpoint and your API key
-GIANT_BOMB_API_ENDPOINT = "https://www.giantbomb.com/api/games/"
-GIANT_BOMB_API_KEY = "7bafcf044a6e38697c4c19b5db7c2f8f085f8f87"  # Replace with your actual Giant Bomb API key
+# Function to fetch games from RAWG API
+@st.cache_data
+def fetch_games(genre):
+    api_key = st.secrets['api_key']
+    url = f"https://api.rawg.io/api/games?key={api_key}&genres={genre}"
+    response = requests.get(url)
+    return response.json()
 
-# Function to fetch the top competitive games from Giant Bomb API
-def fetch_top_games():
-    params = {
-        'api_key': '7bafcf044a6e38697c4c19b5db7c2f8f085f8f87',
-        'format': 'json',
-        'field_list': 'name,site_detail_url',
-        'sort': 'original_release_date:desc',
-        'filter': 'genres:8',  # Adjust the genre filter as needed (8 corresponds to the 'esports' genre)
-        'limit': 10  # Adjust the limit as needed
-    }
-    response = requests.get(GIANT_BOMB_API_ENDPOINT + 'index/', params=params)
-    st.write(response)
-    data = response.json()
-    return data.get('results', [])
+# Streamlit app layout
+st.title('Competitive Video Games Dashboard')
 
-# Streamlit app
-def main():
-    st.title("Top Competitive Video Games")
+# Select genre
+genre = st.selectbox("Select a Game Genre:", ['action', 'strategy', 'shooter', 'sports'])
 
-    # Fetch top competitive games
-    top_games = fetch_top_games()
-
-    # Display the list of games
-    if top_games:
-        st.write("Here are some of the top competitive video games:")
-        for game in top_games:
-            st.write(f"[{game['name']}]({game['site_detail_url']})")
-    else:
-        st.write("No data available. Please check your API key and parameters.")
-
-if __name__ == "__main__":
-    main()
+# Fetch and display games
+data = fetch_games(genre)
+if 'results' in data:
+    games = data['results']
+    for game in games:
+        st.subheader(game['name'])
+        st.write(f"Released: {game['released']}")
+        st.write(f"Rating: {game['rating']}/5")
+        if game['background_image']:
+            st.image(game['background_image'])
